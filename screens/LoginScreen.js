216c09d17ec3268468a3b57983f6aa2991151c70
Philip Lin <philiplin@PhilipdeMacBook-Air.local>
2021-04-21 18:44:39 +0800

    0421 doing

---
+++ screens/LoginScreen.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import {
+    StyleSheet,
+    Text,
+    useColorScheme,
+    View,
+} from 'react-native';
+
+export const LoginScreen = () => {
+    return (
+        <View style={{ flex: 1, backgroundColor: '#137775' }}>
+
+        </View>
+    );
+}
